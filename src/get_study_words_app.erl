c4c275f4cf57bb9bd72a6dc862c675727edbfb07
AtimidKing <y1817629585@163.com>
2018-09-06 22:38:06 +0800

    add static file

--- src/get_study_words_app.erl
+++ src/get_study_words_app.erl
@@ -7,11 +7,13 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
-    Dispatch = cowboy_router:compile([{'_',
-				       [{"/", hello_handler, []}]}]),
+    Dispatch = cowboy_router:compile([
+        {'_',[{"/", hello_handler, []},
+		{"/i", cowboy_static,{priv_file, my_app,"static/index.html"}}
+    ]}]),
     {ok, _} = cowboy:start_clear(my_http_listener,
 				 [{port, 8080}],
 				 #{env => #{dispatch => Dispatch}}),
-    get_study_words_sup:start_link().
+    get_study_words_sup:start_link(). 
 
 stop(_State) -> ok.
